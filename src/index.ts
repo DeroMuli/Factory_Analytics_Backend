8d7ba1cf7c0b7b27f579af514472bc4c3147bc28
DeroMuli <musembimuli1999@gmail.com>
2023-02-13 09:28:31 +0300

    wrapped the cache control header with an if statement

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,14 @@ const startserver = async() => {
 
     app.use(cors());
     app.use(express.json())
-    //prevent caching
-    app.use((req : Request,res : Response,next : NextFunction) => {
-        res.set("Cache-Control", "no-store")
-        next()
-    })
+    //prevent caching to prevent error 304 during realoading of the expo app 
+    if(process.env.NODE_ENV === "development"){
+        app.use((req : Request,res : Response,next : NextFunction) => {
+            res.set("Cache-Control", "no-store")
+            next()
+        })
+    }
+
     app.use("/api/v1",router)
 
     app.listen(port, () => {
